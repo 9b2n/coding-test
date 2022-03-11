95f7a41adfe32eec7859599482c09a1cde811160
9b2n <rbqls1057@gmail.com>
2022-03-12 01:26:40 +0900

    백준 - 바이러스

---
+++ 백준/DFS와 BFS/바이러스.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+
+graph = defaultdict(list)
+n = int(input())
+e = int(input())
+
+visited = [False] * (n+1)
+
+for _ in range(e):
+    s, e = map(int, input().split())
+    graph[s].append(e)
+    graph[e].append(s)
+
+def dfs(graph, v):
+    global visited
+
+    visited[v] = True
+
+    for x in graph[v]:
+        if not visited[x]:
+            dfs(graph, x)
+
+dfs(graph, 1)
+print(graph)
+print(visited)
+
+print(sum(visited)-1)
