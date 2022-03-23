f9cb354d58e0088bc9f72e8ac58ad6f326691c72
9b2n <rbqls1057@gmail.com>
2022-03-23 14:28:07 +0900

    백준 - 최대 힙

---
+++ 백준/우선순위 큐/최대_힙.py
@@ -0,0 +1,14 @@
+from heapq import heappop, heappush
+import sys
+
+n = int(sys.stdin.readline().strip())
+heap = []
+for _ in range(n):
+    x = int(sys.stdin.readline().strip())
+    if x:
+        heappush(heap, (-x, x))
+    else:
+        if heap:
+            print(heappop(heap)[1])
+        else:
+            print(0)
